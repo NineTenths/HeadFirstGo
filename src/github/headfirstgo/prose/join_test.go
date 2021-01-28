c084161d9520242bd346e0034f9d6cd72cfa7de4
David Plescia <dplescia@gmail.com>
2021-01-27 20:08:59 -0500

    Work on testing

--- src/github/headfirstgo/prose/join_test.go
+++ src/github/headfirstgo/prose/join_test.go
@@ -1,17 +1,59 @@
 package prose
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type testData struct {
+	list []string
+	want string
+}
+
+func TestJoinWithCommas(t *testing.T) {
+	tests := []testData{
+		testData{list: []string{"apple"}, want: "apple"},
+		testData{list: []string{"apple", "orange"}, want: "apple and orange"},
+		testData{list: []string{"apple", "orange", "pear"},
+			want: "apple, orange, and pear"},
+	}
+	for _, test := range tests {
+		got := JoinWithCommas(test.list)
+		if got != test.want {
+			t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"",
+				test.list, got, test.want)
+		}
+	}
+}
+
+func TestOneElement(t *testing.T) {
+	list := []string{"apple"}
+	want := "apple"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Error(errorString(list, got, want))
+	}
+}
 
 func TestTwoElements(t *testing.T) {
 	list := []string{"apple", "orange"}
-	if JoinWithCommas(list) != "apple and orange" {
-		t.Error("didn't match expected value")
+	want := "apple and orange"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf(errorString(list, got, want))
 	}
 }
 
 func TestThreeElements(t *testing.T) {
 	list := []string{"apple", "orange", "pear"}
-	if JoinWithCommas(list) != "apple, orange, and pear" {
-		t.Error("didn't match expected value")
+	want := "apple, orange, and pear"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf(errorString(list, got, want))
 	}
+}
+
+func errorString(list []string, got string, want string) string {
+	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"",
+		list, got, want)
 }
